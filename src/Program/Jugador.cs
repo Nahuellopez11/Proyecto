18290df9b45410c9b54ef3b09021d12f37ae5a7d
Manuela Martony <manuela.martony@correo.ucu.edu.uy>
2024-11-06 20:45:51 -0300

    creacion inicial

--- src/Program/Jugador.cs
+++ src/Program/Jugador.cs
@@ -2,26 +2,7 @@ namespace Program;
 
 public class Jugador : IJugadores
 {
-    /*
-    public Dictionary<int, IPokemones> Pokemones { get; set; } = new Dictionary<int, IPokemones>();
-    public List<IPokemones> ListaDePokemones { get; set; } = new List<IPokemones>();
-    public IPokemones pokemonActivo { get; set; }
-    
-    public void ElegirPokemon(int unNum)
-    {
-        IPokemones pokemonElegido = Pokemones[unNum];
-        ListaDePokemones.Add(pokemonElegido);
-        Console.WriteLine($"Has elegido a {pokemonElegido.Nombre}");
-    }
-
-   
-            
-    */
-    
-    
-    
-    
-    
+    public List<Items> ListaItems = new List<Items>();
 
 }
     
