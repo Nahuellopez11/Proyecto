2f63569e9b63bc8de41a8ee6bcee26e3805ee66a
Manuela Martony <manuela.martony@correo.ucu.edu.uy>
2024-11-20 18:29:20 -0300

    ultimos cambios al program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -11,7 +11,27 @@ class Program
     static void Main()
     {
         DemoBot();
+        /*Console.WriteLine("Escoge una opción");
+        Console.WriteLine("1 - Jugar contra máquina");
+        Console.WriteLine("2 - Jugar contra jugador");
+        int opcion=Convert.ToInt32(Console.ReadLine());
         
+
+        if (opcion == 1)
+        {
+            ElegirPokemon.SeleccionarEquipo();
+            ElegirPokemon.SeleccionarEquipoMaquina();
+            IncializacionBatallaContraMaquina.LogicaJuego();
+
+        }
+        else
+        {
+            Console.WriteLine("El Jugador1 elige equipo"); ;
+            ElegirPokemon.SeleccionarEquipo();
+            Console.WriteLine("El Jugador2 elige equipo");
+            ElegirPokemon.SeleccionarEquipo();
+
+        }*/
         
         
  ;
